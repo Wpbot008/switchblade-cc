3c1d404f5c03639dfe64b597d0f21daa509375e0
Wpbot008 <Phonetecno169@gmail.com>
2024-03-13 01:44:26 +0530

    Update switchblade.php

--- switchblade.php
+++ switchblade.php
@@ -6,7 +6,7 @@
 // Webhook: https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
 // BOT API Configuration
-$botToken = "TOKEN"; #<------------------- PUT YOUR TOKEN HERE------------->#
+$botToken = "6415904352:AAHh6wDx96wS_GpH0WgAA9plkJ_lVgpcOC8"; #<------------------- PUT YOUR TOKEN HERE------------->#
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
